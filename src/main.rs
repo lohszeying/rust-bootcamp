53c593bf3cf419eaba55799a0726d0da2a3de9ba
Sze Ying <bloom9334@hotmail.com>
2025-03-29 21:27:23 +0800

    chore: learn implementing structs in rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-struct User {
-    name: String,
-    age: u32,
-    active: bool,
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    fn perimeter(&self) -> u32 {
+        return (self.width + self.height) / 2;
+    }
 }
 
 fn main() {
-    let name = String::from("Alice");
-    let user = User{
-        name,
-        age: 30,
-        active: true,
+    let rect = Rect{
+        width: 30,
+        height: 50,
     };
-    print!("{} is {} years old", user.name, user.age);
+    print!("Area of rectangle is {}", rect.area());
+    print!("Perimeter of rectangle is {}", rect.perimeter());
 }
-
