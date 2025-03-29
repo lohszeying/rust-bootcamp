919c91a4f63a0df211297f9f04c382a156b11a9a
Sze Ying <bloom9334@hotmail.com>
2025-03-30 02:39:23 +0800

    chore: add more things for iterators

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     for val in v1 {
         println!("{}", val);
     }
+    // Note: v1 doesnt work anymore because v1 is using v1.into_iter() under the hood,unless i choose to use v1.iter() in the for loop
+    // println!("v1 is {:?}", v1);
+
+    // ================================================
 
     // Mutating the value in iterator
     // Ownership still belongs to v2, v2_iter is just borrowing it
@@ -21,5 +25,27 @@ fn main() {
         *val = *val + 1;
     }
     println!("{:?}", v2);
+
+    // ================================================
+
+    // The real way an iterator expects you to iterate an array (for loop hides the complexity but this is what's happening under the hood):
+    let nums = vec![1,2,3,4];
+    let mut iter = nums.iter();
+    // while there is still value in the iter.next()
+    while let Some(val) = iter.next() {
+        println!("{}", val); }
+
+    // ================================================
+
+    // into_iter (transfer ownership to v3_iter!)
+    // Can use this if i am sure i wont use v3 anymore. Performance issue! cuz if just use iter() which just borrows, it will use pointer to reference!
+    // if I just write for val in v3, then it actually converts into iter under the hood, and I cant use v3 anymore.
+    let v3 = vec![1,2,3];
+    let v3_iter = v3.into_iter();
+    for val in v3_iter {
+        println!("into_iter val: {}", val);
+    }
 }
+
+
 
