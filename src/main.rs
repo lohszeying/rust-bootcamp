d3e3c58eb9d588cc564b35be05d25dd1eac50aff
Sze Ying <bloom9334@hotmail.com>
2025-03-29 22:35:15 +0800

    chore: add enum and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-struct Rect {
-    width: u32,
-    height: u32,
+enum Direction {
+    North,
+    South,
+    East,
+    West,
 }
 
-impl Rect {
-    fn area(&self) -> u32 {
-        return self.width * self.height;
-    }
+enum Shape {
+    Circle(f64),
+    Square(f64),
+    Rectangle(f64, f64),
+}
 
-    fn perimeter(&self) -> u32 {
-        return (self.width + self.height) / 2;
-    }
+fn calculate_area(shape: Shape) -> f64 {
+    let ans= match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Square(size) => size * size,
+        Shape::Rectangle(width, height) => width * height
+    };
+
+    return ans;
 }
 
 fn main() {
-    let rect = Rect{
-        width: 30,
-        height: 50,
-    };
-    print!("Area of rectangle is {}", rect.area());
-    print!("Perimeter of rectangle is {}", rect.perimeter());
+    let circle = Shape::Circle(5.0);
+    let square = Shape::Circle(4.0);
+    let rectangle = Shape::Rectangle(3.0, 2.0);
+
+    print!("Area of circle: {}", calculate_area(circle));
 }
