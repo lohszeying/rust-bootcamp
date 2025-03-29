aee63ec7f6740416a53b31b4cfc0bbae3020486b
Sze Ying <bloom9334@hotmail.com>
2025-03-30 00:03:06 +0800

    chore: borrowing with mutable

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = &s1;
-    print_str(s2);
+    let mut s1 = String::from("Hello");
+    print_str(&mut s1);
     println!("s1 is {}", s1.to_string())
 }
 
-fn print_str(s: &String) {
-    println!("string is: {}", s);
+fn print_str(s: &mut String) {
+    s.push_str(" world!")
 }
