7a99194890feee964cec1f3f6621c0a60c5236cb
Sze Ying <bloom9334@hotmail.com>
2025-03-31 18:23:29 +0800

    chore: add structs with lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-
+// If the lifetime of name is going to be 'a, then lifetime of User is going to be 'a
+// To let rust compiler know: Whenever name goes out of scope, User goes out of scope
+struct User<'a> {
+    name: &'a str,
+    age: u32,
 }
 
+fn main() {
+    let user;
+    {
+        let first_name = String::from("Jack");
+        user = User {
+            name: &first_name,
+            age: 32,
+        };
+    }
+    println!("First name: {}, age: {}", user.name, user.age);
+}
