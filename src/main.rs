1ab9d3d9fb9d943e79ea85cad49f2a95e8bf8ed7
Sze Ying <bloom9334@hotmail.com>
2025-03-31 19:52:53 +0800

    chore: add message passing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-fn main() {
+use std::sync::mpsc;
+use std::thread;
 
+fn main() {
+    // mpsc is a multi-producer, single-consumer channel
+    // tx is transmitter, rx is receiver
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv();
+
+    // unwrap -> if the value is Ok, it returns the value, if it is Err, it panics
+    // let received = rx.recv().unwrap();
+    // println!("Got: {received}");
+    // unwrap is anti pattern, should not have unwrap in codebase. should have pattern matching, if have err should catch error, etc.
+    match received {
+        Ok(value) => println!("{}", value),
+        Err(e) => println!("Error while reading data: {}", e),
+    }
 }
 
